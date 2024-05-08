67ea2b882043ba61a006e4536085c62ff77d087e
kbouns <bounseng.kevin@gmail.com>
2024-05-08 11:30:13 +0200

    maj

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
       <img className='w-full h-screen object-cover object-contain object-right' src={Test} alt=''/>
       <div className='w-full h-screen absolute top-0 left-0 bg-white/25'>
         <div className='max-w-[1200px] m-auto h-2/3 w-full flex flex-col justify-center lg:items-start items-center'>
-        <h1 className='flex sm:text-4xl text-3xl font-bold text-sky-600'>Je suis 123hhh</h1>
+        <h1 className='flex sm:text-4xl text-3xl font-bold text-sky-600'>Je suis</h1>
         <h2 className='flex sm:text-3xl text-sky-600'>Je suis
           <TypeAnimation
             sequence={[
