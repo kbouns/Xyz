0bd657a3db1ed549d9ea1ad483b60941448798ba
kbouns <bounseng.kevin@gmail.com>
2024-05-16 11:03:13 +0200

    Maj

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-gray-300 p-4">
       <div className="max-w-6xl mx-auto flex justify-center items-center">
-        <p>© 2024 BOUNSENG KEVIN @Inspired by CodeSource. Tous droits réservés.</p>
+        <p>© 2024 BOUNSENG KEVIN Tous droits réservés.</p>
       </div>
     </footer>
   );
